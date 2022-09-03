7032310984ca31e13df6cce6439bd886e0313aaf
tony <nguyenhuytoan1994@outlook.com>
2022-09-03 17:04:11 +0700

    - Implement RESTful API in Go using GIn

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"github.com/toannguyen3105/simplebank/api"
+	db "github.com/toannguyen3105/simplebank/db/sqlc"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:9000"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("Cannot start server:", err)
+	}
+}
